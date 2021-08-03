c7a12fa0021a27438c8c3f803031c73f0af840a4
SabitTheBot <iamsabit99@gmail.com>
2021-08-03 17:34:18 +0600

    add dynamic routing

---
+++ src/routes/pokemon/[id].svelte
@@ -0,0 +1,24 @@
+<script context="module">
+    export async function load({page}){
+        //get the id from url
+        const id =  page.params.id;
+
+        //api
+        const api = `https://pokeapi.co/api/v2/pokemon/${id}`;
+       
+
+        //get specific pokemon information
+        const res =  await fetch(api);
+        const pokemon =  await res.json();
+
+        return {props: {pokemon}};
+    }
+</script>
+
+<script>
+    export let pokemon;
+    let {name} = pokemon;
+</script>
+
+
+<h1 class="text-4xl text-center my-8 uppercase">{name}</h1>
